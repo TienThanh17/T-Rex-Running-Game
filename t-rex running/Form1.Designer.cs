7919152d2326e94b4b62cf069963ef0f9d7ed94c
meoKibo <theboyhunter123@gmail.com>
2022-11-16 21:14:45 +0700

    first commit

---
+++ t-rex running/Form1.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace T_Rex_Running
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtScore = new System.Windows.Forms.Label();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.lblGameOver = new System.Windows.Forms.Label();
+            this.scoreTimer = new System.Windows.Forms.Timer(this.components);
+            this.txtHighScore = new System.Windows.Forms.Label();
+            this.txtPressSpace = new System.Windows.Forms.Label();
+            this.pbxReplay = new System.Windows.Forms.PictureBox();
+            this.t_rex = new System.Windows.Forms.PictureBox();
+            this.pbxGround = new System.Windows.Forms.PictureBox();
+            this.pbxCactus_1 = new System.Windows.Forms.PictureBox();
+            this.pbxCactus_2 = new System.Windows.Forms.PictureBox();
+            this.pbxChim = new System.Windows.Forms.PictureBox();
+            this.pbxCloud_2 = new System.Windows.Forms.PictureBox();
+            this.pbxCloud_1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxReplay)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.t_rex)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxGround)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxCactus_1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxCactus_2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxChim)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxCloud_2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxCloud_1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtScore
+            // 
+            this.txtScore.AutoSize = true;
+            this.txtScore.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtScore.Location = new System.Drawing.Point(12, 44);
+            this.txtScore.Name = "txtScore";
+            this.txtScore.Size = new System.Drawing.Size(79, 23);
+            this.txtScore.TabIndex = 2;
+            this.txtScore.Text = "Score: 0";
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.MainGameTimerEvent);
+            // 
+            // lblGameOver
+            // 
+            this.lblGameOver.AutoSize = true;
+            this.lblGameOver.Font = new System.Drawing.Font("Consolas", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGameOver.Location = new System.Drawing.Point(316, 202);
+            this.lblGameOver.Name = "lblGameOver";
+            this.lblGameOver.Size = new System.Drawing.Size(149, 32);
+            this.lblGameOver.TabIndex = 4;
+            this.lblGameOver.Text = "Game Over";
+            this.lblGameOver.Visible = false;
+            // 
+            // scoreTimer
+            // 
+            this.scoreTimer.Interval = 50;
+            this.scoreTimer.Tick += new System.EventHandler(this.scoreTimerEvent);
+            // 
+            // txtHighScore
+            // 
+            this.txtHighScore.AutoSize = true;
+            this.txtHighScore.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtHighScore.Location = new System.Drawing.Point(12, 9);
+            this.txtHighScore.Name = "txtHighScore";
+            this.txtHighScore.Size = new System.Drawing.Size(125, 23);
+            this.txtHighScore.TabIndex = 7;
+            this.txtHighScore.Text = "High Score: 0";
+            // 
+            // txtPressSpace
+            // 
+            this.txtPressSpace.AutoSize = true;
+            this.txtPressSpace.Font = new System.Drawing.Font("Consolas", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPressSpace.Location = new System.Drawing.Point(259, 185);
+            this.txtPressSpace.Name = "txtPressSpace";
+            this.txtPressSpace.Size = new System.Drawing.Size(299, 32);
+            this.txtPressSpace.TabIndex = 8;
+            this.txtPressSpace.Text = "Press Space To Play";
+            // 
+            // pbxReplay
+            // 
+            this.pbxReplay.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pbxReplay.Image = global::T_Rex_Running.Properties.Resources.replay;
+            this.pbxReplay.Location = new System.Drawing.Point(368, 237);
+            this.pbxReplay.Name = "pbxReplay";
+            this.pbxReplay.Size = new System.Drawing.Size(35, 30);
+            this.pbxReplay.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pbxReplay.TabIndex = 9;
+            this.pbxReplay.TabStop = false;
+            this.pbxReplay.Visible = false;
+            this.pbxReplay.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pbxReplay_MouseClick);
+            // 
+            // t_rex
+            // 
+            this.t_rex.BackColor = System.Drawing.Color.Transparent;
+            this.t_rex.Image = global::T_Rex_Running.Properties.Resources.running;
+            this.t_rex.Location = new System.Drawing.Point(133, 378);
+            this.t_rex.Name = "t_rex";
+            this.t_rex.Size = new System.Drawing.Size(40, 43);
+            this.t_rex.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.t_rex.TabIndex = 3;
+            this.t_rex.TabStop = false;
+            // 
+            // pbxGround
+            // 
+            this.pbxGround.BackColor = System.Drawing.Color.Black;
+            this.pbxGround.Location = new System.Drawing.Point(-9, 421);
+            this.pbxGround.Name = "pbxGround";
+            this.pbxGround.Size = new System.Drawing.Size(818, 47);
+            this.pbxGround.TabIndex = 0;
+            this.pbxGround.TabStop = false;
+            // 
+            // pbxCactus_1
+            // 
+            this.pbxCactus_1.BackColor = System.Drawing.Color.Transparent;
+            this.pbxCactus_1.Image = global::T_Rex_Running.Properties.Resources.obstacle_1;
+            this.pbxCactus_1.Location = new System.Drawing.Point(535, 375);
+            this.pbxCactus_1.Name = "pbxCactus_1";
+            this.pbxCactus_1.Size = new System.Drawing.Size(23, 46);
+            this.pbxCactus_1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pbxCactus_1.TabIndex = 3;
+            this.pbxCactus_1.TabStop = false;
+            this.pbxCactus_1.Tag = "obstacle";
+            // 
+            // pbxCactus_2
+            // 
+            this.pbxCactus_2.BackColor = System.Drawing.Color.Transparent;
+            this.pbxCactus_2.Image = global::T_Rex_Running.Properties.Resources.obstacle_2;
+            this.pbxCactus_2.Location = new System.Drawing.Point(663, 388);
+            this.pbxCactus_2.Name = "pbxCactus_2";
+            this.pbxCactus_2.Size = new System.Drawing.Size(32, 33);
+            this.pbxCactus_2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pbxCactus_2.TabIndex = 3;
+            this.pbxCactus_2.TabStop = false;
+            this.pbxCactus_2.Tag = "obstacle";
+            // 
+            // pbxChim
+            // 
+            this.pbxChim.Image = global::T_Rex_Running.Properties.Resources.chim;
+            this.pbxChim.Location = new System.Drawing.Point(753, 363);
+            this.pbxChim.Name = "pbxChim";
+            this.pbxChim.Size = new System.Drawing.Size(35, 29);
+            this.pbxChim.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pbxChim.TabIndex = 6;
+            this.pbxChim.TabStop = false;
+            this.pbxChim.Tag = "obstacle";
+            // 
+            // pbxCloud_2
+            // 
+            this.pbxCloud_2.Image = global::T_Rex_Running.Properties.Resources.blue_cloud;
+            this.pbxCloud_2.Location = new System.Drawing.Point(454, 303);
+            this.pbxCloud_2.Name = "pbxCloud_2";
+            this.pbxCloud_2.Size = new System.Drawing.Size(68, 26);
+            this.pbxCloud_2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbxCloud_2.TabIndex = 5;
+            this.pbxCloud_2.TabStop = false;
+            this.pbxCloud_2.Tag = "object";
+            // 
+            // pbxCloud_1
+            // 
+            this.pbxCloud_1.Image = ((System.Drawing.Image)(resources.GetObject("pbxCloud_1.Image")));
+            this.pbxCloud_1.Location = new System.Drawing.Point(322, 345);
+            this.pbxCloud_1.Name = "pbxCloud_1";
+            this.pbxCloud_1.Size = new System.Drawing.Size(55, 25);
+            this.pbxCloud_1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pbxCloud_1.TabIndex = 5;
+            this.pbxCloud_1.TabStop = false;
+            this.pbxCloud_1.Tag = "object";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pbxReplay);
+            this.Controls.Add(this.txtPressSpace);
+            this.Controls.Add(this.txtHighScore);
+            this.Controls.Add(this.lblGameOver);
+            this.Controls.Add(this.t_rex);
+            this.Controls.Add(this.pbxGround);
+            this.Controls.Add(this.txtScore);
+            this.Controls.Add(this.pbxCactus_1);
+            this.Controls.Add(this.pbxCactus_2);
+            this.Controls.Add(this.pbxChim);
+            this.Controls.Add(this.pbxCloud_2);
+            this.Controls.Add(this.pbxCloud_1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "T-Rex Running";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyIsDown);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.keyIsPress);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.keyIsUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pbxReplay)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.t_rex)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxGround)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxCactus_1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxCactus_2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxChim)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxCloud_2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxCloud_1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbxGround;
+        private System.Windows.Forms.Label txtScore;
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.PictureBox t_rex;
+        private System.Windows.Forms.PictureBox pbxCactus_1;
+        private System.Windows.Forms.PictureBox pbxCactus_2;
+        private System.Windows.Forms.Label lblGameOver;
+        private System.Windows.Forms.PictureBox pbxCloud_1;
+        private System.Windows.Forms.PictureBox pbxCloud_2;
+        private System.Windows.Forms.PictureBox pbxChim;
+        private System.Windows.Forms.Timer scoreTimer;
+        private System.Windows.Forms.Label txtHighScore;
+        private System.Windows.Forms.Label txtPressSpace;
+        private System.Windows.Forms.PictureBox pbxReplay;
+    }
+}
+
---
+++ t-rex running/Form1.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Media;
+using System.Windows.Forms;
+
+namespace T_Rex_Running
+{
+    public partial class Form1 : Form
+    {
+        private bool jumping = false;
+        private int jumpSpeed = 12;
+        private int force = 12;
+        private int score = 0;
+        private int highScore = 0;
+        private int obstacleSpeed = 10;
+        private Random random = new Random();
+        private bool gameStart = false;
+        private bool gameOver = false;
+        private bool rolling = false;
+        private SoundPlayer jumpSound = new SoundPlayer(@"jump_sound.wav");
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            gameReset();
+        }
+
+        //sự kiện thời gian chính của game
+        private void MainGameTimerEvent(object sender, EventArgs e)
+        {
+            t_rex.Top += jumpSpeed;
+
+            txtScore.Text = "Score: " + score;
+
+            if (jumping && force < 0)
+            {
+                jumping = false;
+            }
+
+            if (jumping)
+            {
+                jumpSpeed = -12;
+                force -= 1;
+            }
+            else
+            {
+                jumpSpeed = 12;
+            }
+
+            //điều chỉnh vị trí của t-rex
+            if (t_rex.Top >= 378 && !jumping)
+            {
+                force = 12;
+                if (rolling) t_rex.Top = 396;
+                else t_rex.Top = 378;
+                jumpSpeed = 0;
+            }
+
+            foreach (Control x in this.Controls)
+            {
+                if (x is PictureBox && (string)x.Tag == "obstacle")
+                {
+                    //thay đổi vị trí của chướng ngại vật
+                    x.Left -= obstacleSpeed;
+
+                    //nếu obstacle đã ra khỏi màn hình thì khởi tạo lại vị trí
+                    if (x.Left < 0)
+                    {
+                        if (x is PictureBox && (string)x.Name == "pbxCactus_1")
+                            x.Left = this.ClientSize.Width + random.Next(1600, 1800);
+
+                        if (x is PictureBox && (string)x.Name == "pbxCactus_2")
+                            x.Left = this.ClientSize.Width + random.Next(1000, 1200);
+
+                        if (x is PictureBox && (string)x.Name == "pbxChim")
+                            x.Left = this.ClientSize.Width + random.Next(100, 300);
+                    }
+
+                    //dừng game khi người chơi chạm chướng ngại vật
+                    if (t_rex.Bounds.IntersectsWith(x.Bounds))
+                    {
+                        gameTimer.Stop();
+                        scoreTimer.Stop();
+                        if (rolling) t_rex.Top = 378;
+                        t_rex.Image = Properties.Resources.dead;
+                        txtScore.Text += "\nPress R to play again !";
+                        lblGameOver.Visible = true;
+                        pbxReplay.Visible = true;
+                        gameOver = true;
+                    }
+                }
+
+                //khởi tạo vị trí cho object(mây)
+                if (x is PictureBox && (string)x.Tag == "object")
+                {
+                    x.Left -= 3;
+                    if (x.Left < 0)
+                        x.Left = this.ClientSize.Width + random.Next(0, 50);
+                }
+            }
+        }
+
+        //hàm bắt đầu game lại từ đầu
+        private void gameReset()
+        {
+            //lưu điểm cao nhất
+            if (highScore < score)
+            {
+                highScore = score;
+                txtHighScore.Text = "High Score: " + highScore;
+            }
+
+            //gán các biến lại giá trị ban đầu
+            txtScore.Text = "Score: " + score;
+            jumping = false;
+            rolling = false;
+            jumpSpeed = 12;
+            force = 12;
+            score = 0;
+            obstacleSpeed = 10;
+            t_rex.Image = Properties.Resources.running;
+            gameOver = false;
+            t_rex.Top = 378;
+            lblGameOver.Visible = false;
+            pbxReplay.Visible = false;
+
+            //khởi tạo vị trí cho obstacle(chướng ngại vật)
+            foreach (Control x in this.Controls)
+            {
+                if (x is PictureBox && (string)x.Tag == "obstacle")
+                {
+                    if (x is PictureBox && (string)x.Name == "pbxCactus_1")
+                        x.Left = this.ClientSize.Width + random.Next(100, 300);
+
+                    if (x is PictureBox && (string)x.Name == "pbxCactus_2")
+                        x.Left = this.ClientSize.Width + random.Next(500, 700);
+
+                    if (x is PictureBox && (string)x.Name == "pbxChim")
+                        x.Left = this.ClientSize.Width + random.Next(900, 1100);
+                }
+            }
+
+            //khi bắt đầu game, Timer của game sẽ khởi động
+            if (gameStart)
+            {
+                gameTimer.Start();
+                scoreTimer.Start();
+            }
+        }
+
+        private void keyIsDown(object sender, KeyEventArgs e)
+        {
+            //đè phím Space hoặc phím ↑ để nhảy
+            if (e.KeyCode == Keys.Space || e.KeyCode == Keys.Up && !jumping)
+            {
+                jumping = true;
+            }
+            //đè phím ↓ để cúi xuống
+            if (e.KeyCode == Keys.Down && !rolling && !gameOver && gameStart)
+            {
+                rolling = true;
+                t_rex.Image = Properties.Resources.rolling;
+            }
+        }
+
+        private void keyIsUp(object sender, KeyEventArgs e)
+        {
+            //nhả phím Space hoặc phím ↑ để hủy nhảy
+            if (jumping)
+            {
+                jumping = false;
+            }
+            //nhả phím ↓ để hủy cúi xuống
+            if (e.KeyCode == Keys.Down && rolling && !gameOver)
+            {
+                rolling = false;
+                t_rex.Image = Properties.Resources.running;
+            }
+            //nhấn phím R để reset game khi game over
+            if (e.KeyCode == Keys.R && gameOver)
+            {
+                gameReset();
+            }
+            //nhấn Space để bắt đầu game
+            if (e.KeyCode == Keys.Space && !gameStart)
+            {
+                gameStart = true;
+                txtPressSpace.Visible = false;
+                gameReset();
+            }
+        }
+
+        private void keyIsPress(object sender, KeyPressEventArgs e)
+        {
+            //Khi t-rex nhảy lên sẽ có âm thanh
+            if (e.KeyChar == 32 || e.KeyChar == 38)
+            {
+                if (!gameOver && t_rex.Top == 378)
+                    jumpSound.Play();
+            }
+        }
+
+        //sự kiện điểm của game
+        private void scoreTimerEvent(object sender, EventArgs e)
+        {
+            score++;
+            //khi điểm là bội số của 500 thì tăng tốc độ của chướng ngại vật
+            if (score % 500 == 0)
+            {
+                obstacleSpeed += 5;
+            }
+        }
+
+        //sự kiên click chuột để reset game
+        private void pbxReplay_MouseClick(object sender, MouseEventArgs e)
+        {
+            gameReset();
+        }
+    }
+}
